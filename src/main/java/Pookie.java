645cecf908b543746c7fa94de62aaa024261cfdd
Dexter Leng <dexterlengcs@gmail.com>
2025-01-24 11:35:23 +0800

    Add echo functionality and exit command to chatbot

--- src/main/java/Pookie.java
+++ src/main/java/Pookie.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Pookie {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
@@ -5,7 +7,23 @@ public class Pookie {
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
-    }
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();    }
 }
